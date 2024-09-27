7f65317aebe6cd30a80a0bced016de252dcf2ccf
pawancisin <pawan.t@cisinlabs.com>
2024-09-27 18:50:11 +0530

    add unstakeByAdmin 27 sep 2024

--- contracts/StakeUnstake.sol
+++ contracts/StakeUnstake.sol
@@ -1,6 +1,162 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+
+    function _contextSuffixLength() internal view virtual returns (uint256) {
+        return 0;
+    }
+}
+
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _nonReentrantAfter();
+    }
+
+    function _nonReentrantBefore() private {
+        // On the first call to nonReentrant, _status will be _NOT_ENTERED
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+    }
+
+    function _nonReentrantAfter() private {
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Returns true if the reentrancy guard is currently set to "entered", which indicates there is a
+     * `nonReentrant` function in the call stack.
+     */
+    function _reentrancyGuardEntered() internal view returns (bool) {
+        return _status == _ENTERED;
+    }
+}
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    /**
+     * @dev The caller account is not authorized to perform an operation.
+     */
+    error OwnableUnauthorizedAccount(address account);
+
+    /**
+     * @dev The owner is not a valid owner account. (eg. `address(0)`)
+     */
+    error OwnableInvalidOwner(address owner);
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
+     */
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(initialOwner);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        if (owner() != _msgSender()) {
+            revert OwnableUnauthorizedAccount(_msgSender());
+        }
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        if (newOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
 interface IERC20 {
     /**
      * @dev Emitted when `value` tokens are moved from one account (`from`) to
@@ -86,7 +242,7 @@ interface IERC20 {
 /// @title StakeUnstake
 /// @notice A contract for staking and unstaking tokens
 /// @dev Implements staking, unstaking, and management of stakers
-contract StakeUnstake {
+contract StakeUnstake is Ownable, ReentrancyGuard {
     bytes private constant PREFIX = "\x19Ethereum Signed Message:\n32";
     address public stblToken;
     address public verifier;
@@ -97,8 +253,15 @@ contract StakeUnstake {
         bool isStaked;
     }
 
+    struct AirdropDetail {
+        uint256 user_id;
+        address recipient;
+    }
+
     //Mapping will return the data of user
     mapping(address => userData) public stakingData;
+
+    mapping(uint256 => bool) public usedBatch;
 
     // Mapping to store staker indices
     mapping(address => uint256) private stakerIndices;
@@ -115,12 +278,21 @@ contract StakeUnstake {
     //events
     event Staked(uint value, address userAddress, uint userId);
     event Unstaked(uint value, address userAddress, uint userId);
+    event UnstakedByAdmin(
+        AirdropDetail[] airdropDetails,
+        uint256 indexed sumAmounts,
+        uint256 indexed batchId
+    );
 
     /**
      * @dev constructor for itializing the contract
      *Assingn the global variables to local variable.
      */
-    constructor(address _verifier, address _stblToken) {
+    constructor(
+        address _owner,
+        address _verifier,
+        address _stblToken
+    ) Ownable(_owner) {
         stblToken = _stblToken;
         verifier = _verifier;
     }
@@ -253,5 +425,39 @@ contract StakeUnstake {
 
         stakers.pop();
         delete stakerIndices[_staker];
+    }
+
+    /**
+     * @dev Function for admin to send tokens to user.
+     * @param airdropDetails users address and their respective claim amount.
+     * @param _sumAmounts total Amounts of tokens claimed by recipients.
+     * @param _batchId maintaining unique batch Id to distinguish staking data.
+     */
+    function unstakeByAdmin(
+        AirdropDetail[] calldata airdropDetails,
+        uint256 _sumAmounts,
+        uint256 _batchId
+    ) public nonReentrant onlyOwner {
+        require(!usedBatch[_batchId], "batch id already used");
+        uint256 contractBalance = IERC20(stblToken).balanceOf(address(this));
+        require(contractBalance >= _sumAmounts, "Insufficient funds");
+
+        // Mark the batch as used and reset total staked
+        usedBatch[_batchId] = true;
+        totalStblStaked -= _sumAmounts;
+
+        for (uint256 i = 0; i < airdropDetails.length; i++) {
+            address recipient = airdropDetails[i].recipient;
+            // Remove the staker and clear their staking data
+            uint256 stakedAmountPerUser = stakingData[recipient].staked;
+            _removeStaker(recipient);
+            stakingData[recipient] = userData({isStaked: false, staked: 0});
+            require(
+                IERC20(stblToken).transfer(recipient, stakedAmountPerUser),
+                "Transfer failed"
+            );
+        }
+
+        emit UnstakedByAdmin(airdropDetails, _sumAmounts, _batchId);
     }
 }
